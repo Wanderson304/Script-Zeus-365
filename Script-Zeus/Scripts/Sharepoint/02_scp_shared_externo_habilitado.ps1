7b45a70e6ce6067dd2ae31d9d3de7415f4d9e675
Wanderson Silva <ca-scudo@hotmail.com>
2024-12-08 23:39:02 -0300

    Zeus 365 - Beta

---
+++ Script-Zeus/Scripts/Sharepoint/02_scp_shared_externo_habilitado.ps1
@@ -0,0 +1,43 @@
+﻿
+Function 02_scp_shared_externo_habilitado {
+
+             
+               #Listar sites que estão com compartilhamento externo habilitados
+
+             #Todos os sites do SharePoint
+             Connect-SPOService -url https://$dominio-admin.sharepoint.com
+
+             Get-SPOSite | Select-object Title, url,Status, Owner, sharingcapability | Export-CSV -LiteralPath $destinoExport_Report_shared_externo_habilitado\tbl_shared_externo_habilitado.csv -NoTypeInformation -Encoding UTF8
+
+             Clear-Host
+             menu-sharepoint
+
+             Write-Host " " 
+             Write-Host "Um momento estamos gerando seu relatório..." -ForegroundColor Yello
+             Write-Host " " 
+
+             Start-Sleep -Seconds 10
+			 Write-Host " " 
+             animacao_espera
+
+             Clear-Host
+             menu-sharepoint
+
+             Write-Host " " 
+             Write-Host "Relatório gerado... Ele será exibido em 10 segundos!" -ForegroundColor Yello
+
+             Write-Host " " 
+             Start-Sleep -Seconds 10
+             Write-Host " " 
+             animacao_espera
+
+             02_scp_shared_externo_habilitado
+
+             #Caminho de exportação
+             $destinoExport_Report_shared_externo_habilitado = "C:\Users\WandersonSilva\OneDrive - Bravo GRC\Negocios\WeS Operacional\Departamentos\T.I\Projetos interno Wes Tech\Script-Zeus\01 - Dev\ETL\01_crg_sharepoint\02_Report_shared_externo_habilitado"
+
+             start $destinoExport_Report_shared_externo_habilitado\Report_lista_links_compartilhados_extern.xlsm
+             menu-sharepoint
+             
+        }
+             
---
+++ Script-Zeus/Scripts/Sharepoint/04_scp_todos_usuarios_externos.ps1
@@ -0,0 +1,69 @@
+﻿
+#Read more: https://www.sharepointdiary.com/2017/11/sharepoint-online-find-all-external-users-using-powershell.html#ixzz8tnV4gzbB
+
+
+function report_todos_usuarios_externos {
+
+
+#Template
+
+#Import SharePoint Online Management Shell
+Import-Module Microsoft.Online.Sharepoint.PowerShell -DisableNameChecking
+
+
+$UrlLogin = "https://$dominio-admin.sharepoint.com" 
+
+#Dominio em letras maiusculas
+$DiminioUpper = $dominio.ToUpper()
+ 
+#Config Parameters
+$AdminSiteURL=$UrlLogin
+$ReportOutput ="$destinoExport_Report_todos_usuarios_externos\tbl_todos_usuarios_externos.csv"
+ 
+#Get Credentials to connect
+#$Cred = Get-Credential
+ 
+#Connect to SharePoint Online Tenant Admin
+Connect-SPOService -URL $AdminSiteURL 
+ 
+#Get All Site Collections
+$SiteCollections  = Get-SPOSite -Limit All
+ 
+#Iterate through each site collection and get external users
+
+Write-host
+Write-host "Um momento vamos iniciar a varredura de usuários externos no dominio" $DiminioUpper -ForegroundColor Yello
+Write-host "Porem, isso pode demorar até 25 minutos" -ForegroundColor Yello
+Write-host
+Start-Sleep -Seconds 8
+
+Foreach ($Site in $SiteCollections)
+
+{   
+    Clear-Host
+    baner-meu-sharepoint
+    mostarDominio
+    Write-host
+
+    Write-host "Pesquisando por usuários externos no dominio $DiminioUpper :" -ForegroundColor Yello
+
+    Write-host
+    animacao_espera
+    Write-host
+
+    #Write-host -f Green "Checking Site Collection:"$Site.URL
+    Try {
+        For ($x=0;;$x+=50) {
+            $ExternalUsers += Get-SPOExternalUser -SiteUrl $Site.Url -Position $x -PageSize 50 -ErrorAction Stop | Select DisplayName,EMail,InvitedBy,AcceptedAs,WhenCreated,@{Name = "SiteUrl" ; Expression = {$Site.url}
+        }
+    }
+}
+catch {}
+}
+ 
+#Export the Data to CSV file
+$ExternalUsers | Export-Csv -Path $ReportOutput -NoTypeInformation
+
+}
+
+
